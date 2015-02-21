783ded4ce7f5e32f346a935371329c616d9d1261
David Sancho <kroneos@gmail.com>
2015-02-21 19:09:18 +0100

    Clase del sabado 21/02/2015

---
+++ app/src/androidTest/java/es/uem/david/samuel/nacho/yepnsd/LoginTest.java
@@ -0,0 +1,63 @@
+package es.uem.david.samuel.nacho.yepnsd;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.text.Editable;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.ParseUser;
+
+/**
+ * Created by david.sancho on 21/02/2015.
+ *
+ * @author david.sancho
+ */
+public class LoginTest extends ActivityInstrumentationTestCase2<LoginActivity> {
+
+    private static final String USERNAME = "D A V I D";
+    private static final String PASSWORD = "D A V I D";
+
+    private LoginActivity actividad;
+    private EditText username;
+    private EditText password;
+    private Button login;
+
+    public LoginTest() {
+        super(LoginActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        actividad = getActivity();
+        username = (EditText) actividad.findViewById(R.id.usernameField);
+        password = (EditText) actividad.findViewById(R.id.passwordField);
+        login = (Button) actividad.findViewById(R.id.loginbutton);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testLogin() {
+        // Primero hacemos logout
+        ParseUser.logOut();
+
+        // Rellenamos los campos
+        TouchUtils.tapView(this, username);
+        sendKeys(USERNAME);
+
+        TouchUtils.tapView(this, password);
+        sendKeys(PASSWORD);
+
+        // Hacemos click al boton de login
+        TouchUtils.clickView(this, login);
+
+        // Comprobamos que tenemos usuario
+        ParseUser pUser = ParseUser.getCurrentUser();
+        assertNotNull(pUser);
+    }
+}
